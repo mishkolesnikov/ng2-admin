7a2eaa752a7277329b8514f9ed1e341285d143bc
Alexander Zhukov <a.zhukov@akveo.com>
2017-04-10 17:23:10 +0300

    In progress

--- src/framework/theme/components/menu/menu.options.ts
+++ src/framework/theme/components/menu/menu.options.ts
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
 import { List } from 'immutable';
 
 export class NgaMenuItem {
--- src/framework/theme/components/menu/menu.service.ts
+++ src/framework/theme/components/menu/menu.service.ts
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
 import { Injectable, Optional } from '@angular/core';
 import { Router } from '@angular/router';
 import { Routes } from '@angular/router';
